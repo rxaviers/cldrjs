3e982b3d25b82fbe2cb47c0d35f4407c9598f174
Rafael Xavier de Souza <rxaviers@gmail.com>
2014-10-14 18:16:47 -0300

    Build

---
+++ dist/node_main.js
@@ -0,0 +1,22 @@
+/**
+ * CLDR JavaScript Library v0.3.9
+ * http://jquery.com/
+ *
+ * Copyright 2013 Rafael Xavier de Souza
+ * Released under the MIT license
+ * http://jquery.org/license
+ *
+ * Date: 2014-10-14T21:16Z
+ */
+/*!
+ * CLDR JavaScript Library v0.3.9 2014-10-14T21:16Z MIT license © Rafael Xavier
+ * http://git.io/h4lmVg
+ */
+
+// Cldr
+module.exports = require( "./cldr" );
+
+// Extent Cldr with the following modules
+require( "./cldr/event" );
+require( "./cldr/supplemental" );
+require( "./cldr/unresolved" );
